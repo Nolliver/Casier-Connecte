5330dae68fbf3d0aad0694aaf6fa01d51ddd53f1
Nolliver <64092458+Nolliver@users.noreply.github.com>
2021-03-08 23:45:14 +0100

    navbar recherche

--- recherche.php
+++ recherche.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php	
+<?php
 	if (isset($_GET['table']) and isset($_GET['sous_categorie'])){
 		echo "<html lang='fr'>\n";
 	} else {
@@ -16,15 +16,79 @@
 
 	<link rel="stylesheet" type="text/css" href="recherche.css">
 
+	<style type="text/css">
+		/* ============ desktop view ============ */
+		@media all and (min-width: 992px) {
+			.navbar .nav-item .dropdown-menu{ display: none; }
+			.navbar .nav-item:hover .nav-link{ color: #fff;  }
+			.navbar .nav-item:hover .dropdown-menu{ display: block; }
+			.navbar .nav-item .dropdown-menu{ margin-top:0; }
+		}
+		/* ============ desktop view .end// ============ */
+	</style>
 </head>
+
 <body class="min-vh-100">
+
+	<nav class="sticky-top navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container-fluid justify-content-end">
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarScroll">
+				<ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="index.php">Acceuil</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="ajout.php">Ajouter</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="recherche.php" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Rechercher
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+							<li><a class="dropdown-item" href="recherche.php">Rechercher</a></li>
+							<li><hr class="dropdown-divider"></li>
+							<li><a class="dropdown-item" href="recherche.php?table=quincaillerie">Quincaillerie</a></li>
+							<li><a class="dropdown-item" href="recherche.php?table=outils">Outils</a></li>
+							<li><a class="dropdown-item" href="recherche.php?table=composants_electroniques">Composants éléctroniques</a></li>
+							<li><a class="dropdown-item" href="recherche.php?table=elements_mecaniques">Elements mécaniques</a></li>
+						</ul>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="edit_del.php" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+							Modifier/Supprimer
+						</a>
+						<ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+							<li><a class="dropdown-item" href='edit_del.php'>Modifier/Supprimer</a></li>
+							<li><hr class="dropdown-divider"></li>
+							<li><a class="dropdown-item" href='edit_del.php?table=quincaillerie'>Quincaillerie</a></li>
+							<li><a class="dropdown-item" href="edit_del.php?table=outils">Outils</a></li>
+							<li><a class="dropdown-item" href="edit_del.php?table=composants_electroniques">Composants éléctroniques</a></li>
+							<li><a class="dropdown-item" href="edit_del.php?table=elements_mecaniques">Elements mécaniques</a></li>
+						</ul>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href=verif.php>Vérification</a>
+					</li>
+				</ul>
+			</div>
+			<div class="mx-auto order-0">
+				<a class="navbar-brand mx-auto" href="#">Casier Connécté</a>
+			</div>
+			<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+			</div>
+		</div>
+	</nav>
+
 	<h1 class="d-block d-sm-none">XS</h1>
 	<h1 class="d-none d-sm-block d-md-none">SM</h1>
 	<h1 class="d-none d-md-block d-lg-none">MD</h1>
 	<h1 class="d-none d-lg-block d-xl-none">LG</h1>
 	<h1 class="d-none d-xl-block">XL</h1>
 
-			<?php 
+			<?php
 
 			include("connexion.php");
 			include("fonction.php");
@@ -35,7 +99,7 @@
 						$categ = array(
 							array('nom_table' => "elements_mecaniques", 'Titre' => "Elements mecaniques" , 'nom_photo' => "elements_mecaniques.jpg"),
 							array('nom_table' => "composants_electroniques", 'Titre' => "Composants electroniques" , 'nom_photo' => "composants_electroniques.jpg"),
-							array('nom_table' => "outils", 'Titre' => "Outils" , 'nom_photo' => "outils.jpg"), 
+							array('nom_table' => "outils", 'Titre' => "Outils" , 'nom_photo' => "outils.jpg"),
 							array('nom_table' => "quincaillerie", 'Titre' => 'Quincaillerie' , 'nom_photo' => 'quincaillerie.jpg')
 						);
 
@@ -120,7 +184,7 @@
 							$result = $bdd -> query($sql.$filtre.' Order by 2, 3;');
 							$result = $result->fetchAll(PDO::FETCH_ASSOC);
 						}
-							
+
 					// Affichage des produits
 						echo "<div class = container-fluid>\n";
 							echo "<div class='row col-12 p-0 m-0'>\n";
@@ -175,7 +239,7 @@
 									echo "</form>\n";
 
 									//Affichage des filtres selectionnés
-									echo "<div class='col-12 my-3 px-0'>";	
+									echo "<div class='col-12 my-3 px-0'>";
 										echo "<div class='card mt-5'>";
 											echo "<div class='card-body px-0 py-3 w-auto'>";
 												echo "<ul>\n";
@@ -186,9 +250,9 @@
 										echo "</div>\n";
 									echo "</div>\n";
 								echo "</div>\n";
-								
 
-								
+
+
 
 							//Remplissage du tableau
 								echo "<div class='table-responsive-lg col-10'>\n";
@@ -217,7 +281,7 @@
 						echo "</div>\n";
 						echo $js_filtre;
 					}
-	 			
+
 			 ?>
 			</div>
 				<a role="button" href="index.php" class="offset-3 col-6 btn btn-outline-info my-5">Retour à l'acceuil</a>
